3fc24c0f17cea5d3427a19250e5f51716bb640a4
dmitriy.arefev <dmitriy.arefev@restream.ru>
2026-06-08 14:45:00 +0300

    hello layout example

--- lib/hello_web/controllers/hello_html.ex
+++ lib/hello_web/controllers/hello_html.ex
@@ -2,4 +2,12 @@ defmodule HelloWeb.HelloHTML do
   use HelloWeb, :html
 
   embed_templates "hello_html/*"
+
+  attr :messanger, :string, required: true
+
+  def greet(assigns) do
+    ~H"""
+    <h2>Привет от {@messanger}!</h2>
+    """
+  end
 end
