c413975a47e4004ffcacbf555e2a480355d1854b
dmitriy.arefev <dmitriy.arefev@restream.ru>
2026-06-10 14:32:54 +0300

    plug PromEx.Plug, prom_ex_module: HelloWeb.PromEx, path: "/metrics"

--- lib/hello_web/endpoint.ex
+++ lib/hello_web/endpoint.ex
@@ -1,6 +1,8 @@
 defmodule HelloWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :hello
 
+  plug PromEx.Plug, prom_ex_module: HelloWeb.PromEx, path: "/metrics"
+
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
@@ -14,8 +16,6 @@ defmodule HelloWeb.Endpoint do
   socket "/live", Phoenix.LiveView.Socket,
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
-
-  plug PromEx.Plug, prom_ex_module: HelloWeb.PromEx, path: "/metrics"
 
   # Serve at "/" the static files from "priv/static" directory.
   #
